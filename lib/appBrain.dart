e3f9892bbc2577cbcc29ad1c86de0b925b6fabdb
Farah <farahmohap111@gmail.com>
2024-02-20 13:16:02 -0800

    first commit

---
+++ lib/appBrain.dart
@@ -0,0 +1,13 @@
+class AgoraManager {
+static String get appId {
+return 'b4e0549a2ce140588540be96d61ffed5';
+}
+
+static String get token {
+return '007eJxTYLgze+GqKYv/6bY+tNiVzWG02a/G9Xv7t5X1t9foZcXsvl+hwJBkkmpgamKZaJScamhiYGphYWpikJRqaZZiZpiWlppiemL6ldSGQEaGFxmrmRgZIBDE52BIzkjMy0vNMWRgAADHmyRi';
+}
+
+static String get channelName {
+return 'channel1';
+}
+}
